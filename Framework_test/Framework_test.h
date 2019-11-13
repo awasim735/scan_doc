cd8059bc368a06d9396d725cb8eb8423e5ab20d9
Waseem Akram <waseem.mohemmed@thomsonreuters.com>
2019-11-13 15:20:03 +0530

    added whole code

---
+++ Framework_test/Framework_test.h
@@ -0,0 +1,20 @@
+//
+//  Framework_test.h
+//  Framework_test
+//
+//  Created by Thomson Reuters on 12/11/19.
+//  Copyright © 2019 Thomson Reuters. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework_test.
+FOUNDATION_EXPORT double Framework_testVersionNumber;
+
+//! Project version string for Framework_test.
+FOUNDATION_EXPORT const unsigned char Framework_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework_test/PublicHeader.h>
+
+
+
